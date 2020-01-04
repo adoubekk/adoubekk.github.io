454e6def9d34d19d41010d75dc2096c6ab7787a2
adoubekk <adoubekk@ucsc.edu>
2020-01-04 14:05:03 -0800

    Added barebones YAML->HTML resume generation

---
+++ generate_resume.py
@@ -0,0 +1,19 @@
+"""
+generate_resume.py
+
+Author: adoubekk
+
+Generates my resume as HTML for use in my personal website.
+"""
+
+from Cheetah.Template import Template
+import yaml
+
+with open('resume.yaml') as resume_yaml_file:
+    resume_data = yaml.full_load(resume_yaml_file.read())
+
+with open('templates/resume.html') as html_template_file:
+    html_template = html_template_file.read()
+
+with open('index.html', 'w') as html_out_file:
+    html_out_file.write(str(Template(html_template, searchList=resume_data)))
